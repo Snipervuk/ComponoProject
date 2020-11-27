64a495976a4b8650cdd08d837e5dc03e89207939
Snipervuk <43171677+Snipervuk@users.noreply.github.com>
2020-11-28 00:35:11 +1000

    update

--- croomo-compono-unity-test-project-f73825219f79/Assets/Scripts/ExportScripts/ExportRoutine.cs
+++ croomo-compono-unity-test-project-f73825219f79/Assets/Scripts/ExportScripts/ExportRoutine.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.AddressableAssets;
 
+
+/// <summary>
+/// Description: Script to use for taking photos and exporting them. By the use of userinput and goes through the hierarchy of SpawnList 
+/// By: Christian Milicevic
+/// Date: 27/11/202
+/// </summary>
 public class ExportRoutine : MonoBehaviour
 {
     //Their Inbuilt Vars
@@ -11,15 +17,15 @@ public class ExportRoutine : MonoBehaviour
     //My Vars
     float rotationAmount = 22.5f;
     float rotateObj;
-    bool hasRotated = false;
-    bool takenPhoto = false;
-    bool allScreenshotsTaken = false;
+
     bool startingPhotoTaken = false;
-    bool endPhotoTaken = false;
     public Vector3 StartingEulerAngles;
+
     public int photoIndexCounter = 0;
     public int itemListNum = 0;
+    public string objectNameGet;
 
+    //Called Before Start
     private void Awake()
     {
         //Set Rotate Obj by rotationAmount
@@ -29,13 +35,29 @@ public class ExportRoutine : MonoBehaviour
         StartingEulerAngles = transform.position;
     }
 
+    //Called First Frame
+    private void Start()
+    {
+        if (m_itemList == null || m_itemList.AssetReferenceCount == 0)
+        {
+            Debug.LogError("Spawn list not setup correctly");
+        }
+        LoadItemAtIndex(m_itemList, itemListNum);
+    }
+
+    //Called Every Frame
     private void Update()
     {
-        //currentEulerAngles = this.transform.rotation.eulerAngles;
+        //Gets object name when it changes
+        objectNameGet = m_instanceObject.name;
 
         RotateObjectandScreenCap();
     }
 
+    /// <summary>
+    /// Rotation and Screenshot function for the objects
+    /// To use press the SpaceBar function on the keyboard!
+    /// </summary>
     void RotateObjectandScreenCap()
     {
         //SpaceBar Function
@@ -51,19 +73,26 @@ public class ExportRoutine : MonoBehaviour
             if (photoIndexCounter >= 10)
             {
                 //4 digits double number output
-                ScreenCapture.CaptureScreenshot(@"Assets\Outputs\Car-1203-Black\frame00" + photoIndexCounter.ToString() + ".png");
+                //ScreenCapture.CaptureScreenshot(@"Assets\Outputs\Car-1203-Black\frame00" + photoIndexCounter.ToString() + ".png");
+                ScreenCapture.CaptureScreenshot(@"Assets\Outputs\" + objectNameGet.ToString() + "\frame00" + photoIndexCounter.ToString() + ".png");
+
             }
             else //Photo frame num is 9 or below
             {   //4 Digits single number output
-                ScreenCapture.CaptureScreenshot(@"Assets\Outputs\Car-1203-Black\frame000" + photoIndexCounter.ToString() + ".png");
-            }
+
+                //ScreenCapture.CaptureScreenshot(@"Assets\Outputs\Car-1203-Black\frame000" + photoIndexCounter.ToString() + ".png");
+                ScreenCapture.CaptureScreenshot(@"Assets\Outputs\" + objectNameGet.ToString() + "\frame000" + photoIndexCounter.ToString() + ".png");
+            }  
         }
 
         //At Last Photo
-        if (photoIndexCounter == 17 && allScreenshotsTaken == false)
+        if (photoIndexCounter == 17)
         {
             //Increament itemListNum for the SpawnList
             itemListNum++;
+
+            //Reset for starting photo
+            startingPhotoTaken = false;
 
             //Destroy AND LOADS Load New Object / Increment 
             LoadItemAtIndex(m_itemList, itemListNum);
@@ -71,24 +100,18 @@ public class ExportRoutine : MonoBehaviour
             //Reset
             photoIndexCounter = 0;
 
-            Debug.Log("Photos finshed for this object, moving onto the next one!");
+            //For Testing
+            Debug.Log("Photos finished for this object, moving onto " + objectNameGet);
         }
-    }
-
-    private void Start()
-    {
-        if (m_itemList == null || m_itemList.AssetReferenceCount == 0)
-        {
-            Debug.LogError("Spawn list not setup correctly");
-        }
-        LoadItemAtIndex(m_itemList, itemListNum);
     }
 
     private void LoadItemAtIndex(SpawnItemList itemList, int index)
     {
         if (m_instanceObject != null)
         {
+            /*
             Destroy(m_instanceObject);
+            */
         }
 
         m_assetLoadedAsset = itemList.GetAssetReferenceAtIndex(index);
@@ -103,11 +126,16 @@ public class ExportRoutine : MonoBehaviour
         {
             m_instanceObject = Instantiate(obj.Result, spawnPosition, spawnRotation, parentTransform);
 
-            //First Screenshot goes here and increment counter
-            
+            //Set name, so object can be stored in correct Output folder
+            m_instanceObject.name = obj.Result.name;
+
             if (StartingEulerAngles == new Vector3(0, 0, 0) && startingPhotoTaken == false)
             {
-                ScreenCapture.CaptureScreenshot(@"Assets\Outputs\Car-1203-Black\frame000" + photoIndexCounter.ToString() + ".png");
+                //First Screenshot goes here and increment counter
+                //ScreenCapture.CaptureScreenshot(@"Assets\Outputs\Car-1203-Black\frame000" + photoIndexCounter.ToString() + ".png");
+
+                ScreenCapture.CaptureScreenshot(@"Assets\Outputs\" + objectNameGet.ToString() + "a" + "frame000" + photoIndexCounter.ToString() + ".png");
+
                 photoIndexCounter++;
                 startingPhotoTaken = true;
             }
